f4b368c545abbf63efe77a623ed617b14f798147
Kirill Zikevskiy <2309696@mail.ru>
2021-02-26 15:48:40 +0300

    feat: make popup open and popup close feature

--- script/index.js
+++ script/index.js
@@ -1,2 +1,10 @@
-let X;
-dfr
+let popup = document.querySelector('.popup');
+let popupOpenButton = document.querySelector('.profile__edit-btn');
+let popupCloseButton = document.querySelector('.popup__form-close-btn');
+
+function popupOpenClose() {
+  popup.classList.toggle('popup_opened');
+}
+
+popupOpenButton.addEventListener('click', popupOpenClose);
+popupCloseButton.addEventListener('click', popupOpenClose);
