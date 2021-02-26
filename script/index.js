168e4778102cade00f654ba5e212c96a3a867430
Kirill Zikevskiy <2309696@mail.ru>
2021-02-26 23:00:58 +0300

    feat: make profile edit logic

--- script/index.js
+++ script/index.js
@@ -1,10 +1,25 @@
 let popup = document.querySelector('.popup');
 let popupOpenButton = document.querySelector('.profile__edit-btn');
 let popupCloseButton = document.querySelector('.popup__form-close-btn');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let form = document.querySelector('.popup__form')
+let input = document.querySelectorAll('.popup__form-input');
 
 function popupOpenClose() {
   popup.classList.toggle('popup_opened');
 }
 
+function editProfile(evt) {
+  evt.preventDefault();
+  let name = input[0].value;
+  let description = input[1].value;
+  profileName.textContent = name;
+  profileDescription.textContent = description;
+  popupOpenClose();
+}
+
+
 popupOpenButton.addEventListener('click', popupOpenClose);
+form.addEventListener('submit', editProfile);
 popupCloseButton.addEventListener('click', popupOpenClose);
